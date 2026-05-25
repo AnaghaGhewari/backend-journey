2d640698b912d2b57fa3215767b736847bca02a8
AnaghaGhewari <anaghaghewari13xv@gamil.com>
2026-05-25 17:19:52 +0530

    Building a mock server step by step

--- week1/day5/01_hello_server.py
+++ week1/day5/01_hello_server.py
@@ -12,16 +12,22 @@
 #       Content-Type: application/json
 #
 #       {"heart_rate": 88}
-
+import json
 
 #---Let's start---
 def do_self(self):
-    if self.path =="vitals":
+    if self.path =="/vitals":
         self.send_response(200)
         self.send_header("Content-Type","application/json")
         self.end_headers()
         self.wfile.write(json.dumps({"heart_rate":88}).encode())
-        
+
+# Fast api does the same thing
+@app.get("/vitals")
+def get_vitals():
+    return{"heart_rate":88}
+
+
 
 
 
---
+++ week1/day5/02_routes.py
@@ -0,0 +1,21 @@
+#Building a mock server step by step
+
+#STEP - 1: Simplest Possible Server
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type","application/json")
+        self.end_headers()
+        body = json.dumps({"message":"VitalGuard is live"})
+        self.wfile.write(body.encode())
+
+    def log_message(self, format, *args):
+       pass
+server = HTTPServer(("localhost",8000),SimpleHandler)   
+print("Running -> http://localhost:8000")
+server.serve_forever() 
